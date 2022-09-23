bda84acc22986075997b6aab44c193b8b434e177
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2022-09-23 11:44:58 -0500

    Added\ Logo Freetravelers

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -43,7 +43,8 @@ const logout = () => {
                             <!-- Logo -->
                             <div class="shrink-0 flex items-center">
                                 <Link :href="route('dashboard')">
-                                    <ApplicationMark class="block h-9 w-auto" />
+                                    <ApplicationMark class="block h-10 w-auto" />
+                                    <!-- <img src="/assets/freetravelers-logo.jpg" class="block h-9 w-auto" alt="Logo Freetravelers"> -->
                                 </Link>
                             </div>
 
