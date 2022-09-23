911255a2205ca26ca4bda58452c2e4f473be45a7
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2022-09-23 17:36:38 -0500

    Hotfix\ Modifed Nabva

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -50,11 +50,9 @@ const logout = () => {
 
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
-                                </NavLink>
+
                                 <NavLink :href="route('home')" :active="route().current('home')">
-                                    Home
+                                    Cotizador
                                 </NavLink>
                             </div>
                         </div>
