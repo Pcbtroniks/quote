58028693fbe0f7e76c143f16dc91e6fb7c2e72e2
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2022-11-02 15:48:02 -0600

    Added\ Pickups tabñe

---
+++ database/migrations/2022_11_02_154547_create_pickups_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pickups', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('hotel_id')
+                    ->constrained()
+                    ->cascadeOnUpdate()
+                    ->cascadeOnDelete();
+
+            $table->foreignId('activity_id')
+                    ->constrained()
+                    ->cascadeOnUpdate()
+                    ->cascadeOnDelete();
+
+            $table->foreignId('zone_id')
+                    ->nullable()
+                    ->constrained()
+                    ->cascadeOnUpdate()
+                    ->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pickups');
+    }
+};
