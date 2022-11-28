9ac88fc30d2a4a9cdf985439b3d3730b1f095135
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2022-11-27 20:44:42 -0600

    Change\ Date format

--- app/Models/QuoteActivity.php
+++ app/Models/QuoteActivity.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\Dateable;
 
 class QuoteActivity extends Model
 {
-    use HasFactory;
+    use HasFactory, Dateable;
 
     protected $table = 'quote_activity';
 
@@ -42,4 +44,17 @@ class QuoteActivity extends Model
         return $this->belongsTo(Hotel::class, 'hotel_id', 'id');
      
     }
+
+    /**
+     * Date Translation
+     * @return string
+    */
+    public function date(): Attribute{
+
+        return Attribute::make(
+            get: fn ($value) => $this->toLocaleDateStringES($value),
+        );
+
+    }
+
 }
