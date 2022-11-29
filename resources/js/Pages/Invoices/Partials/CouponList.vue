4224d92276d2da9b7636cecb95b6f2938e9fccbe
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2022-11-29 17:21:46 -0600

    Add\ Coupon link in invoice coupons route

--- resources/js/Pages/Invoices/Partials/CouponList.vue
+++ resources/js/Pages/Invoices/Partials/CouponList.vue
@@ -83,7 +83,7 @@ try {
                 <tbody>
                 <tr v-for="coupon in props.coupons.data">
                     <th class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
-                        {{ coupon.code }}
+                        <a :href="coupon.quote.url">{{ coupon.code }}</a>
                     </th>
                     <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 ">
                         {{ coupon.status }}
