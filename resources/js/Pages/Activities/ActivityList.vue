1cea01b9f961851f5aadeb884991c10b7bdb84a3
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2023-02-10 13:04:16 -0600

    Add\ EDit icon to ft admins in costos page

--- resources/js/Pages/Activities/ActivityList.vue
+++ resources/js/Pages/Activities/ActivityList.vue
@@ -91,7 +91,7 @@ const isFilter = (origin, type) => {
                             <tr v-for="activity in props.activities.data" class="hover:cursor-pointer hover:text-white hover:bg-sky-400 h-10">
                                 <td class="p-2 whitespace-nowrap">
                                     <div class="flex items-center">
-                                        <div @click="useEditActivity(activity)" class="font-medium">{{ activity.name }}</div>
+                                        <div @click="useEditActivity(activity)" class="font-medium"><img v-if="$page.props.user.is_freetraveler_admin" class="w-3 inline mr-1 stroke-sky-500 fill-sky-500 text-sky-500" src="/assets/icons/edit-icon.svg" alt="Edit Icon">{{ activity.name }}</div>
                                     </div>
                                 </td>
                                 <td class="p-2 whitespace-nowrap">
