82a350e99f091705cea574ee157e1eddf5ff2951
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2023-02-28 14:03:05 -0600

    Fix\ QuoteList
    Fix\ Quote #  Pack activities & Promomex tag

--- resources/js/Pages/Home/QuoteList.vue
+++ resources/js/Pages/Home/QuoteList.vue
@@ -157,9 +157,9 @@ const printTempFolio = (quote) => {
                                     {{ quote.listed_activities[0].date }}
                                 </td>
                                 <td class="p-2 whitespace-nowrap">
-                                    <div class="text-left font-medium text-green-500 capitalize" :title="quote.type == 'entrada' && quote.national == '1' ? 'Promomex' : ''">
-                                        {{ quote.type }} <span v-if="quote.type == 'paquete'">#{{quote.listed_activities.length}}</span>
-                                        <span v-if="quote.type == 'entrada' && quote.national == '1'">
+                                    <div class="text-left font-medium text-green-500 capitalize" :title="quote.type == 'entrance' && quote.national == '1' ? 'Promomex' : ''">
+                                        {{ quote.type }} <span v-if="quote.type == 'pack'">#{{quote.listed_activities.length}}</span>
+                                        <span v-if="quote.type == 'entrance' && quote.national == '1'">
                                             <svg title="Promomex" class="inline" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
                                                 <path d="M9.16667 2.5L16.6667 10C17.0911 10.4745 17.0911 11.1922 16.6667 11.6667L11.6667 16.6667C11.1922 17.0911 10.4745 17.0911 10 16.6667L2.5 9.16667V5.83333C2.5 3.99238 3.99238 2.5 5.83333 2.5H9.16667" stroke="#52525B" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
                                                 <circle cx="7.50004" cy="7.49967" r="1.66667" stroke="#52525B" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></circle>
