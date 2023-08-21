0e7f257c3150d09bdfae76776d4d7ac0099f94fe
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2023-08-21 16:47:14 -0600

    Add\ Reduce size of notes text area in CreateFormQuoter placeholder adde

--- resources/js/Pages/Quoter/partials/CreateFormQuote.vue
+++ resources/js/Pages/Quoter/partials/CreateFormQuote.vue
@@ -780,7 +780,7 @@ const Activities = reactive(new postActivities());
                                     <textarea
                                     v-model="form.notas"
                                     class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                                    name="note" id="notes" cols="30" rows="1" placeholder="notas..."></textarea>
+                                    name="note" id="notes" cols="30" rows="1" placeholder="Notas (opcional)"></textarea>
                                 </div>
                             </div>
 
