e2d91cc0ab3826d56822c35dc514a4687630f060
Jean <jeanmacario048@gmail.com>
2024-03-20 16:40:13 -0600

    Add\ NOT VARIABLE BY AGENCY/TEAM currency exchange

---
+++ database/migrations/2024_03_20_152349_create_currencies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('conversion_rate', 10, 4)->default(1.0);
+            $table->string('code', 3);
+            $table->string('name');
+            $table->string('symbol', 5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+};
