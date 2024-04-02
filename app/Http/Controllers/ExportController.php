cdf7bde90aaa4c5c16ce85d3f58785741d777d79
Jean <jeanmacario048@gmail.com>
2024-04-02 16:50:21 -0600

    Build\ Logo By agency 02.04.2024-1650

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -79,6 +79,9 @@ class ExportController extends Controller
 
     public function exportPDFQuoteDownlaod(Quote $quote)
     {
+        $quote->logo = ($quote->team->logo == '\\assets\\freetravelers-logo.jpg')
+        ? public_path('assets/freetravelers-logo.jpg')
+        : public_path('/storage/' . $quote->team->logo);
         Pdf::setOption(['dpi' => 150, 'defaultFont' => 'sans-serif']);
         $pdf = Pdf::loadView('Exports.Quote', ['quote' => $quote]);
         return $pdf->download('freetravelers-quote.pdf');
---
+++ public/build/assets/TeamHelpers.8fb9f139.js
@@ -0,0 +1,1 @@
+const s=e=>e=="\\assets\\freetravelers-logo.jpg",o=e=>e||"/images/agency/no-logo.png",r={isDefaultLogo:s,ParseAgencyLogo:o};export{r as T,s as i};
