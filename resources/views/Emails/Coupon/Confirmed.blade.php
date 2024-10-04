0d8bdc541802d5e2943ec73935455cd598a8a340
Jean <jeanmacario048@gmail.com>
2024-10-04 17:20:19 -0600

    Build\ ExploreEmotions email request coupon fix 04.10.2024-1720

--- resources/views/Emails/Coupon/Confirmed.blade.php
+++ resources/views/Emails/Coupon/Confirmed.blade.php
@@ -293,7 +293,7 @@ ul.social li{
           	<table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
           		<tr>
           			<td class="logo" style="text-align: center;">
-			            <h1><a href="#">FreeTravelers</a></h1>
+			            <h1><a href="#">Explore Emotions</a></h1>
 			          </td>
           		</tr>
           	</table>
